73b73a9fad4bf37acc65c0038446d8ae8feabf8a
guilhermecoelho <guilhermecoelho2@gmail.com>
2014-07-27 23:22:13 -0300

    local CRUD

---
+++ meivorts/Scripts/pages/Local.js
@@ -0,0 +1,11 @@
+﻿// show or hide form by checkBox
+function showEnderecoForm(valor) {
+
+    if (valor == 0) {
+        $("#enderecoForm").show();
+        $("#showEndereco").val(1);
+    } else {
+        $("#enderecoForm").hide();
+        $("#showEndereco").val(0);
+    }
+}
